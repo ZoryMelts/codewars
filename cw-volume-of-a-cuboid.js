43e9256f169b142a810d2782dbacf80662242223
Zoey Melf <zoeymelf@gmail.com>
2022-05-14 12:48:29 -0500

    volume of a cuboid

---
+++ cw-volume-of-a-cuboid.js
@@ -0,0 +1,21 @@
+// Volume of a cuboid 8kyu
+
+// Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+      return length*width*height;
+      // your code here
+    }
+  }
+
+//   tests
+
+// describe("Tests", () => {
+//     it("test", () => {
+//   Test.assertEquals(Kata.getVolumeOfCuboid(1,2,2), 4);
+//   Test.assertEquals(Kata.getVolumeOfCuboid(6.3,2,5), 63);
+//     });
+//   });
+  
+  
