d6c130c6c2d7323058936498f6d46e96b484cae8
Zoey Melf <zoeymelf@gmail.com>
2022-05-14 09:07:11 -0500

    add files from 5-10 through 5-14

---
+++ cw-5-14-2022.js
@@ -0,0 +1,25 @@
+// Fix the following function
+
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+// Example output:
+
+// Hello, Mr. Spock
+
+function sayHello(name) {
+    return (`Hello, ${name}`)
+  }
+
+// tests
+
+describe("Tests", () => {
+    it("test", () => {
+  Test.assertEquals(sayHello('Mr. Spock'), 'Hello, Mr. Spock')
+  Test.assertEquals(sayHello('Captain Kirk'), 'Hello, Captain Kirk')
+  Test.assertEquals(sayHello('Liutenant Uhura'), 'Hello, Liutenant Uhura')
+  Test.assertEquals(sayHello('Dr. McCoy'), 'Hello, Dr. McCoy')
+    });
+  });
+  
+
+  
