700096e0e893be24d82fa9a16c0944f7835c04df
Zoey Melf <zoeymelf@gmail.com>
2022-05-16 11:54:43 -0500

    Twice as old, codewars 8kyu

---
+++ cw-5-16-2022.js
@@ -0,0 +1,6 @@
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    // let differenceAge = dadYearsOld - sonYearsOld;
+    // return  Math.abs(differenceAge - sonYearsOld);
+     
+     return Math.abs(dadYearsOld - (2*sonYearsOld));
+   }
